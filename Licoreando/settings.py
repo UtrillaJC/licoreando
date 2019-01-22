42025eb9063b8df1ce4a847083031282cf6d508f
davidromesp <davidromesp@hotmail.com>
2019-01-22 05:24:42 +0100

    nud

--- Licoreando/settings.py
+++ Licoreando/settings.py
@@ -116,10 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-LANGUAGES = (
-    ('es', _('Español')),
-)
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
